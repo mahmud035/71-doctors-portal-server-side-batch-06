e79bfb24d9ab528ecb4d89efaa82ecd0c939ab06
mahmud035 <mahamudulhasan4148@gmail.com>
2022-11-15 20:02:58 +0600

    basic server setup and connected to Mongodb Atlas

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('colors');
+require('dotenv').config();
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+//* Middleware
+app.use(cors());
+app.use(express.json());
+
+//* Mongodb Atlas
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yeflywl.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log(uri);
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const dbConnect = async () => {
+  try {
+    await client.connect();
+    console.log('Database connected'.yellow.italic);
+  } catch (error) {
+    console.log(error.name.bgRed, error.message.bold);
+  }
+};
+
+dbConnect();
+
+//* Collection
+
+app.get('/', (req, res) => {
+  res.send('doctors portal server is running');
+});
+
+app.listen(port, () => {
+  console.log('Server up and running'.cyan.bold);
+});
